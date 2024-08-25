ddad97632703a5ad2a478c8d17ad35fb9d84be42
Dindokar-Prajakta12 <dindokarprajakta28@gmail.com>
2024-08-25 12:51:22 +0530

    first commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    const handleBookRoomClick = () => {
+        navigate('/rooms'); // Navigate to the '/book-room' route
+    };
+
     return (
         <div className="home-container">
             <h1>SAWATA GUEST HOUSE</h1>
             <p>Every house guest brings you happiness..</p>
-            <p> Some when they arrive, and Some when they are leaving</p>
+            <p>Some when they arrive, and Some when they are leaving</p>
             <p>(here you will get comfy rooms)</p>
-            <button className="book-room-button">Book a Room</button>
+            <button className="book-room-button" onClick={handleBookRoomClick}>Book a Room</button>
         </div>
     );
 };
 
 export default Home;
+
